aaaee410ee65270ed547794eba09c87b663f8b50
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-04-21 21:41:18 -0400

    added credits and level end trigger

---
+++ Assets/Scripts/SceneBranch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneBranch : MonoBehaviour {
+
+    public string[] scenes = new string[2];
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(scenes[index]);
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+}
